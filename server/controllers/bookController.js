23bf032edd7f8e5e3801d3d2e23c8a99f1f24146
Mithesh2004 <mitheshm2004@gmail.com>
2025-04-25 20:23:24 +0000

    change book controller

--- server/controllers/bookController.js
+++ server/controllers/bookController.js
@@ -1,50 +1,68 @@
-import crypto from "crypto"
-import Book from "../models/BookModel"
+import crypto from "crypto";
+const { PinataSDK } = require("pinata");
+const fs = require("fs");
+const { Blob } = require("buffer");
+import Book from "../models/BookModel";
+require("dotenv").config();
 
-const ipfs = ipfsHttpClient({ url: 'https://ipfs.infura.io:5001/api/v0' })
-const MASTER_KEY = process.env.MASTER_KEY || "supersecret"
+const MASTER_KEY = process.env.MASTER_KEY || "supersecret";
 
 const encryptBufferAES = (buffer, key) => {
-    const iv = crypto.randomBytes(16)
-    const cipher = crypto.createCipheriv('aes-256-cbc', Buffer.from(key, 'hex'), iv)
-    const encrypted = Buffer.concat([cipher.update(buffer), cipher.final()])
-    return { encryptedData: encrypted, iv }
-}
+  const iv = crypto.randomBytes(16);
+  const cipher = crypto.createCipheriv(
+    "aes-256-cbc",
+    Buffer.from(key, "hex"),
+    iv
+  );
+  const encrypted = Buffer.concat([cipher.update(buffer), cipher.final()]);
+  return { encryptedData: encrypted, iv };
+};
+const uploadipfs = async (encryptedData, fileName) => {
+  const pinata = new PinataSDK({
+    pinataJwt: process.env.PINATA_JWT,
+    pinataGateway: process.env.GATEWAY_URL,
+  });
+  const blob = new Blob([encryptedData], { type: "application/octet-stream" });
+  const file = new File([blob], fileName);
+  const upload = await pinata.upload.public.file(file);
+  console.log("IPFS Upload:", upload);
+  return upload;
+};
 
 const uploadBook = async (req, res) => {
+  try {
+    const { title, author, genre, year } = req.body;
+    const file = req.file;
+    if (!file) return res.status(400).json({ error: "No file uploaded" });
 
-    try {
-        const { title, author, genre, year } = req.body
-        const file = req.file
-        if (!file) return res.status(400).json({ error: 'No file uploaded' })
+    const aesKey = crypto.randomBytes(32).toString("hex");
+    const { encryptedData, iv } = encryptBufferAES(file.buffer, aesKey);
 
+    const ipfsResult = await uploadToIPFS(encryptedData, `${title}.enc.pdf`);
+    const cid = ipfsResult.cid;
 
-        const aesKey = crypto.randomBytes(32).toString('hex')
-        const { encryptedData, iv } = encryptBufferAES(file.buffer, aesKey)
+    const encryptedAesKey = crypto
+      .createCipher("aes-256-cbc", MASTER_KEY)
+      .update(aesKey, "utf8", "hex");
 
-        const ipfsResult = await ipfs.add(encryptedData)
-        const cid = ipfsResult.path
+    const bookTokenId = 0;
 
-        const encryptedAesKey = crypto.createCipher('aes-256-cbc', MASTER_KEY).update(aesKey, 'utf8', 'hex')
+    const newBook = new Book({
+      title,
+      author,
+      genre,
+      year,
+      ipfscid: cid,
+      aesEncryptedKey: encryptedAesKey,
+      bookTokenId,
+    });
 
-        const bookTokenId = await mintNft(title)
+    await newBook.save();
+    res.json({ success: true, bookId: newBook._id });
+  } catch (error) {
+    console.error(err);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
 
-        const newBook = new Book({
-            title,
-            author,
-            genre,
-            year,
-            ipfscid: cid,
-            aesEncryptedKey: encryptedAesKey,
-            bookTokenId
-        })
-
-        await newBook.save()
-        res.json({ success: true, bookId: newBook._id })
-    } catch (error) {
-        console.error(err)
-        res.status(500).json({ error: 'Internal Server Error' })
-    }
-}
-
-export { uploadBook }
+export { uploadBook };
